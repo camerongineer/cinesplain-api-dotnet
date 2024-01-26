7ec8fd4bad57b21b56c702182b1d03bce4d1d023
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-26 11:38:23 -0800

    Chore: Set LogoPath to nullable

--- CineSplain.API/Models/TMBD/ProductionCompany.cs
+++ CineSplain.API/Models/TMBD/ProductionCompany.cs
@@ -2,7 +2,7 @@ namespace CineSplain.API.Models.TMBD;
 
 public class ProductionCompany {
     public int Id { get; set; }
-    public string LogoPath { get; set; }
+    public string? LogoPath { get; set; }
     public string Name { get; set; }
     public string OriginCountry { get; set; }
 }
