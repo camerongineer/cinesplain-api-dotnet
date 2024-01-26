800d23f5d49a0412df30164e29412a519366203d
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-26 11:37:40 -0800

    Fix: Change Iso6391 to Iso_639_1

--- CineSplain.API/Models/TMBD/SpokenLanguage.cs
+++ CineSplain.API/Models/TMBD/SpokenLanguage.cs
@@ -2,6 +2,6 @@ namespace CineSplain.API.Models.TMBD;
 
 public class SpokenLanguage {
     public string EnglishName { get; set; }
-    public string Iso6391 { get; set; }
+    public string Iso_639_1 { get; set; }
     public string Name { get; set; }
 }
