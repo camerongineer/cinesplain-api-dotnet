808380a26dfb22076ef08a44ffff7ee1d10ccbaa
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-29 10:23:43 -0800

    refactor: Modify GetPerson to use FullDisplayPerson class

--- CineSplain.API/Program.cs
+++ CineSplain.API/Program.cs
@@ -5,6 +5,7 @@ DotNetEnv.Env.Load();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOutputCache();
 
 var app = builder.Build();
 
@@ -24,5 +25,7 @@ app.UseCors(options => {
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseOutputCache();
 
 app.Run();
