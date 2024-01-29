ffba5da1942c9a5111067fff30550f245efb290b
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-29 10:18:40 -0800

    refactor: rename CreditCategory to MovieCreditCategory

--- CineSplain.API/Models/TMBD/MovieCreditCategory.cs
+++ CineSplain.API/Models/TMBD/MovieCreditCategory.cs
@@ -1,6 +1,6 @@
 ﻿namespace CineSplain.API.Models.TMBD;
 
-public class CreditCategory {
+public class MovieCreditCategory {
     public List<CastMember> Cast { get; set; }
     public List<CrewMember> Crew { get; set; }
 }
