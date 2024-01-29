aa8ce710af2420a104a6b8539241619938b27ae7
Cameron Marr <alphaomegabeta@gmail.com>
2024-01-29 10:17:23 -0800

    chore: set iso_639_1 to nullable

--- CineSplain.API/Models/TMBD/Image.cs
+++ CineSplain.API/Models/TMBD/Image.cs
@@ -3,7 +3,7 @@
 public class Image {
     public double AspectRatio { get; set; }
     public int Height { get; set; }
-    public string Iso_639_1 { get; set; }
+    public string? Iso_639_1 { get; set; }
     public string FilePath { get; set; }
     public double VoteAverage { get; set; }
     public int VoteCount { get; set; }
