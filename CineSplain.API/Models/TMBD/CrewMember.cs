8a8307899d1d9a93b1e44a4d7677948098f90501
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-21 12:36:35 -0800

    chore: modify to extend Person class and implement ICrewCredit

--- CineSplain.API/Models/TMBD/CrewMember.cs
+++ CineSplain.API/Models/TMBD/CrewMember.cs
@@ -1,15 +1,11 @@
 ﻿namespace CineSplain.API.Models.TMBD;
 
-public class CrewMember {
-    public bool Adult { get; set; }
+public class CrewMember : Person, ICrewCredit {
     public string CreditId { get; set; }
     public string Department { get; set; }
     public int Gender { get; set; }
-    public int Id { get; set; }
     public string KnownForDepartment { get; set; }
     public string Job { get; set; }
     public string Name { get; set; }
     public string OriginalName { get; set; }
-    public double Popularity { get; set; }
-    public string ProfilePath { get; set; }
 }
