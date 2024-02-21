ba5416463be7255ba86f274df3a5e8f43ac26567
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-21 12:29:23 -0800

    chore: add ICrewCredit interface

---
+++ CineSplain.API/Models/TMBD/ICrewCredit.cs
@@ -0,0 +1,8 @@
+﻿namespace CineSplain.API.Models.TMBD;
+
+public interface ICrewCredit {
+    int Id { get; set; }
+    public string CreditId { get; set; }
+    string Job { get; set; }
+    string Department { get; set; }
+}
