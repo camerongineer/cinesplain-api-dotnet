25a9320da270606372f72c04953e4b114385053b
Cameron Marr <alphaomegabeta@gmail.com>
2024-02-21 12:35:54 -0800

    chore: modify to extend ICrewCredit interface

--- CineSplain.API/Models/TMBD/ListDisplayMovieCrewCredit.cs
+++ CineSplain.API/Models/TMBD/ListDisplayMovieCrewCredit.cs
@@ -2,7 +2,7 @@
 
 namespace CineSplain.API.Models.TMBD;
 
-public class ListDisplayMovieCrewCredit: ListDisplayMovie {
+public class ListDisplayMovieCrewCredit: ListDisplayMovie, ICrewCredit {
     [Required] public string CreditId { get; set; }
     public string Department { get; set; }
     public string Job { get; set; }
